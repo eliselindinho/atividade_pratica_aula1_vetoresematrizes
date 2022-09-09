842c3a0380ba9f9cfd09626118a3df7ada782293
elis mendonca <elis_pm@hotmail.com>
2022-09-09 15:56:25 -0300

    Incluindo e consultando código de produtos

---
+++ js/aula1.js
@@ -0,0 +1,37 @@
+let armazenaLista = [
+  ["0", "sem cadastro"],
+  ["1", "leite"],
+  ["2", "ovo"],
+  ["3", "farinha"],
+  ["4", "milho"],
+  ["5", "café"],
+];
+
+let recebeCodigo = prompt(
+  "Digite a funcionalidade desejada: (incluir ou consultar)"
+).toLowerCase();
+
+let mensagem = "";
+
+switch (recebeCodigo) {
+  case "consultar":
+    recebeCodigo = prompt("Digite o código do produto que deseja encontrar: ");
+    let encontrouProduto = " ";
+    for (let codigo of armazenaLista) {
+      encontrouProduto = codigo.indexOf(recebeCodigo);
+      if (encontrouProduto != -1) {
+        mensagem = `Lista:${codigo[0]} ${codigo[1]} ${codigo[2]}`;
+      } else {
+        mensagem = "Código não encontrado!";
+      }
+    }
+    break;
+
+  case "incluir":
+    let codigo = prompt("Digite o código");
+    let produto = prompt("Digite o produto");
+    let incluiLista = [codigo, produto];
+    armazenaLista.push(incluiLista);
+    mensagem = "Lista incluída com sucesso:${codigo} ${produto}";
+    break;
+}
